17fcf3fbff9a2fcdb61dc54f7b90e7f9a2e446a8
Matic Jurglic <matic@jurglic.si>
2015-11-06 21:29:31 +0100

    Add an optional 'automatic_pageview_tracking' flag

--- app/instance-initializers/mixpanel.js
+++ app/instance-initializers/mixpanel.js
@@ -3,9 +3,11 @@ import Config from '../config/environment';
 export function initialize(instance) {
   if (Config.mixpanel.enabled) {
     var router = instance.container.lookup('router:main');
-    router.on('didTransition', function() {
-      instance.container.lookup('service:mixpanel').trackPageView(this.get('url'));
-    });
+    if (Config.mixpanel.automatic_pageview_tracking == undefined || Config.mixpanel.automatic_pageview_tracking) {
+      router.on('didTransition', function() {
+        instance.container.lookup('service:mixpanel').trackPageView(this.get('url'));
+      });
+    }
   }
 }
 
